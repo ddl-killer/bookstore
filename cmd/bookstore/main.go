d1395a828db95dc29f43c3d6bb534bde30f7660a
ddl-killer <1754009643@qq.com>
2022-08-23 00:19:38 +0800

    add：HTTP 服务模块

--- cmd/bookstore/main.go
+++ cmd/bookstore/main.go
@@ -1,7 +1,15 @@
 package bookstore
 
 import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
 	_ "bookstore/internal/store"
+	"bookstore/server"
 
 	"bookstore/store/factory"
 )
@@ -11,4 +19,33 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
+	srv := server.NewBookStoreServer(":8080", s)
+	errChan, err := srv.ListenAndServe()
+	if err != nil {
+		log.Println("web server start failed:", err)
+		return
+	}
+	log.Println("web server start ok")
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+
+	select {
+	case err = <-errChan:
+		log.Println("web server run failed:", err)
+		return
+	case <-c:
+		log.Println("bookstore program is exiting...")
+		ctx, cf := context.WithTimeout(context.Background(), time.Second)
+		defer cf()
+		srv.Shutdown(ctx)
+	}
+
+	if err != nil {
+		log.Println("bookstore program exit error:", err)
+		return
+	}
+	log.Println("bookstore program exit ok")
+
 }
--- server/server.go
+++ server/server.go
@@ -1,1 +1,108 @@
 package server
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"bookstore/server/middleware"
+
+	"github.com/gorilla/mux"
+
+	"bookstore/store"
+)
+
+type BookStoreServer struct {
+	s   store.Store
+	srv *http.Server
+}
+
+func NewBookStoreServer(addr string, s store.Store) *BookStoreServer {
+	srv := &BookStoreServer{
+		s: s,
+		srv: &http.Server{
+			Addr: addr,
+		},
+	}
+
+	router := mux.NewRouter()
+	router.HandleFunc("/book", srv.createBookHandler).Methods(http.MethodPost)
+	router.HandleFunc("/book/{id}", srv.updateBookHandler).Methods(http.MethodPost)
+	router.HandleFunc("/book/{id}", srv.getBookHandler).Methods(http.MethodGet)
+	router.HandleFunc("/book", srv.getAllBooksHandler).Methods(http.MethodGet)
+	router.HandleFunc("/book/{id}", srv.delBookHandler).Methods(http.MethodDelete)
+
+	srv.srv.Handler = middleware.Logging(middleware.Validating(router))
+	return srv
+}
+
+func (bs *BookStoreServer) createBookHandler(w http.ResponseWriter, req *http.Request) {
+	dec := json.NewDecoder(req.Body)
+	var book store.Book
+	if err := dec.Decode(&book); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if err := bs.s.Create(&book); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	return
+}
+
+func (bs *BookStoreServer) updateBookHandler(w http.ResponseWriter, req *http.Request) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (bs *BookStoreServer) getBookHandler(w http.ResponseWriter, req *http.Request) {
+	id, ok := mux.Vars(req)["id"]
+	if !ok {
+		http.Error(w, "no id found in request", http.StatusBadRequest)
+		return
+	}
+
+	book, err := bs.s.Get(id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	response(w, book)
+}
+
+func response(w http.ResponseWriter, v interface{}) {
+	data, err := json.Marshal(v)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(data)
+}
+
+func (bs *BookStoreServer) getAllBooksHandler(w http.ResponseWriter, req *http.Request) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (bs *BookStoreServer) delBookHandler(w http.ResponseWriter, req *http.Request) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (bs *BookStoreServer) ListenAndServe() (<-chan error, error) {
+	var err error
+	errChan := make(chan error)
+	go func() {
+		bs.srv.ListenAndServe()
+		errChan <- err
+	}()
+
+	select {
+	case err = <-errChan:
+		return nil, err
+	case <-time.After(time.Second):
+		return errChan, nil
+	}
+}
